b0995f7c370c19e0beeb810984f057dedaa7be74
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-08-30 04:02:46 +0000

    增加JAVA接口请求功能

---
+++ trunk/QT/Qt/DES_encrypt_app/DES_Qt/YLW_key_value.h
@@ -0,0 +1,50 @@
+﻿/******************************************************************************
+ * Copyright(c) 2015 Daboo Technology Co.,Ltd.
+ *
+ * @FileName:    keyvalue.h
+ * @Author:      pengxuejin
+ * @Version:     1.4
+ * @Date:        2015.8.4
+ * @Description: http请求需要发送到服务器的数据格式化处理辅助类
+ * @Others:
+ * @History:
+ *     1、Date:         2015.8.4
+ *        Author:       pengxuejin
+ *        Modification: 基本功能添加
+ *     2、Date:         2015.8.17
+ *        Author:       pengxuejin
+ *        Modification: 给出一个查找某个key对应字符串的接口以供日志上传模块使用
+ *     3、Date:         2015.8.19
+ *        Author:       pengxuejin
+ *        Modification: 头文件中模块说明添加
+ *     4、Date:         2015.8.20
+ *        Author:       pengxuejin
+ *        Modification: 代码小调整
+*****************************************************************************/
+#ifndef KEYVALUE_H
+#define KEYVALUE_H
+
+#include <map>
+#include <QString>
+#include "YLW_vs_char_set.h"
+
+typedef std::map<QString, QString> MapQStringtoQString;
+
+class KeyValue
+{
+public:
+    KeyValue();
+
+    void putString(const QString &strKey, const QString &strValue);
+    void setConnector(const QString &strConnector);
+    void setDelimiter(const QString &strDelimiter);
+    QString toQString();
+    QString getStringValue(const QString &strKey);
+
+private:
+    QString m_strConnector;
+    QString m_strDelimiter;
+    MapQStringtoQString m_mapData;
+};
+
+#endif // KEYVALUE_H
