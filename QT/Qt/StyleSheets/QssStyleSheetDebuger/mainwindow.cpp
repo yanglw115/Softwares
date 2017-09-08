af05b3ecf44bc2ca10302db8b5331a4d328ffa50
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-09-08 03:49:26 +0000

    为了方便调试与制作QSS，专门创建一个图形界面项目用于调试QSS

---
+++ QT/Qt/StyleSheets/QssStyleSheetDebuger/mainwindow.cpp
@@ -0,0 +1,38 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    m_pFileSystemModel = new QFileSystemModel(this);
+    m_pFileSystemModel->setRootPath("D:\\");
+    m_pStringListModel = new QStringListModel(this);
+    QStringList strList;
+    strList << "Vim" << "Source Insight" << "Android Stdio" << "Eclips" << "QT";
+    m_pStringListModel->setStringList(strList);
+    ui->listView->setModel(m_pStringListModel);
+    ui->treeView->setModel(m_pFileSystemModel);
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            ui->tableWidget->setItem(i, j, new QTableWidgetItem(QString("%1, %2").arg(i).arg(j)));
+        }
+    }
+    QStringList strListColumn;
+    for (int i = 0; i < 200; ++i) {
+        strListColumn << QString("Column %1").arg(i);
+    }
+    ui->columnView->setModel(new QStringListModel(strListColumn, this));
+    connect(ui->listView, SIGNAL(doubleClicked(QModelIndex)), this, SLOT(slotChangeStacked(QModelIndex)));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::slotChangeStacked(const QModelIndex &index)
+{
+    ui->stackedWidget->setCurrentIndex(index.row());
+}
