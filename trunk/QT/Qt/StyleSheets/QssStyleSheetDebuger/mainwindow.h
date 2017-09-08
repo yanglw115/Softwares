968a41a42e16c94839103d72c987affac1c4f3f6
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-09-08 03:49:26 +0000

    为了方便调试与制作QSS，专门创建一个图形界面项目用于调试QSS

---
+++ trunk/QT/Qt/StyleSheets/QssStyleSheetDebuger/mainwindow.h
@@ -0,0 +1,27 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QFileSystemModel>
+#include <QStringListModel>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+private slots:
+    void slotChangeStacked(const QModelIndex &index);
+private:
+    Ui::MainWindow *ui;
+    QFileSystemModel *m_pFileSystemModel;
+    QStringListModel *m_pStringListModel;
+};
+
+#endif // MAINWINDOW_H
