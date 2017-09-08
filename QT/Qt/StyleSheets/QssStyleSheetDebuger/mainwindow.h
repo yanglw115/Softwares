2eaaea5bb152f2203b118ccbcd7b9409478dfe65
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-09-08 08:16:10 +0000

    增加QSS样式文件改变监听与刷新

--- QT/Qt/StyleSheets/QssStyleSheetDebuger/mainwindow.h
+++ QT/Qt/StyleSheets/QssStyleSheetDebuger/mainwindow.h
@@ -4,6 +4,7 @@
 #include <QMainWindow>
 #include <QFileSystemModel>
 #include <QStringListModel>
+#include <QFileSystemWatcher>
 
 namespace Ui {
 class MainWindow;
@@ -16,12 +17,17 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+
+    void registerFileWatcher(const QString &strFilePath);
+public slots:
+    void slotWatchedFileChanged(const QString &strFilePath);
 private slots:
     void slotChangeStacked(const QModelIndex &index);
 private:
     Ui::MainWindow *ui;
     QFileSystemModel *m_pFileSystemModel;
     QStringListModel *m_pStringListModel;
+    QFileSystemWatcher *m_pFileSystemWatcher;
 };
 
 #endif // MAINWINDOW_H
