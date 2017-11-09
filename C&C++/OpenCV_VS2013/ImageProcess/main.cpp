e03b16046e8087dd87fb54416c9980cd7811ef57
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-11-09 09:09:09 +0000

    face spots detect.

--- C&C++/OpenCV_VS2013/ImageProcess/main.cpp
+++ C&C++/OpenCV_VS2013/ImageProcess/main.cpp
@@ -2,10 +2,11 @@
 
 #include "showImage.h"
 #include "dftImage.h"
+#include "imageProcess.h"
 
 using namespace cv;
 
-void main()
+void main1()
 {
 	//showImage("images/3.jpg");
 	//ircodeImage("3.jpg");
@@ -15,6 +16,7 @@ void main()
 	//imageROI("images/img2.jpg", "images/logo.jpg");
 	//imageSplit("images/dota_jugg.jpg", "images/dota_logo.jpg");
 	//setContrastAndBright("images/dota_jugg.jpg");
-	dftImage("images/dota_jugg.jpg");
+	//dftImage("images/dota_jugg.jpg");
+	imageFilter("images/AC.jpg");
 }
 
