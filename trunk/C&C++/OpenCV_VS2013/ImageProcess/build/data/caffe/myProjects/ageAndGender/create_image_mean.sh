eb3a38f1d1e29e9e4a0b5f25253357db9cf78811
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-11-17 02:06:05 +0000

    create image mean脚本上传

---
+++ trunk/C&C++/OpenCV_VS2013/ImageProcess/build/data/caffe/myProjects/ageAndGender/create_image_mean.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+CaffeRoot=/home/yangliwei/caffe/caffe-master
+ProjectDir=myProjects/ageAndGender
+DataDir=${CaffeRoot}/${ProjectDir}/data
+
+meanTool=${CaffeRoot}/build/tools/compute_image_mean
+trainLmdb=${DataDir}/image_train_lmdb
+meanFileName=mean.binaryproto
+
+rm -f ${DataDir}/${meanFileName}
+
+
+echo "Start to generate mean binaryproto..."
+${meanTool} ${trainLmdb} ${DataDir}/${meanFileName}
+echo "Done."
