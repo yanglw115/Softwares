e53cc0eb21add9f1ea372322a4ef46c114816f90
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-11-20 09:09:46 +0000

    简单的JNI接口封装

---
+++ C&C++/OpenCV_VS2013/FaceCheck/CureFaceParser.cpp
@@ -0,0 +1,15 @@
+
+#include "CureFaceParser.h"
+
+#include <iostream>
+
+using namespace std;
+
+JNIEXPORT jstring JNICALL Java_CureFaceParser_getFaceParseResult
+  (JNIEnv *env, jobject obj, jstring strFilePath)
+{
+	const char *pStrFilePath = env->GetStringUTFChars(strFilePath, 0);
+	cout << "Input parameter:" << pStrFilePath;
+	env->ReleaseStringUTFChars(strFilePath, 0);
+	return env->NewStringUTF("Hello JNI.");
+}
