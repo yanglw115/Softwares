8499c6613c85fc5316ddb0380627430ceebefa90
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-11-22 10:11:16 +0000

    upgrade package.sh

--- C&C++/OpenCV_VS2013/FaceCheck/build/package/package.sh
+++ C&C++/OpenCV_VS2013/FaceCheck/build/package/package.sh
@@ -1,15 +1,39 @@
 #!/bin/bash
 
-PWD=$(pwd)
-NameLibsPackage=libs.tar.gz
-DirLibs=${PWD}/../libs
-DirLibStdcpp=${PWD}/../libstdc++.so.6.0.24
-DirLibJNILib=${PWD}../../libCureFaceParser.so
-
-rm -rf ${DirLibs}/${NameLibsPackage}
-
 set -o nounset  #脚本中禁止使用未定义的变量。或使用set -u
 set -o errexit  #脚本中任意一行执行错误时则退出。或使用set -e
+
+PWD=$(pwd)
+NamePackage=package.tar.gz
+NameStdcpp=libstdc++.so.6.0.24
+NameJNILib=libCureFaceParser.so
+NameFaceLandmarks=shape_predictor_68_face_landmarks.dat
+DirLibs=${PWD}/../libs
+DirLibStdcpp=${PWD}/../${NameStdcpp}
+DirLibJNILib=${PWD}/../../${NameJNILib}
+DirFaceLandmarks=${PWD}/../../${NameFaceLandmarks}
+
+read -p "Do you have copyed opencv and dlibs libraries to their directory? [Y/N]: " REPLY
+
+if [ -z ${REPLY} ] || [ ${REPLY}x = "Y"x ] || [ ${REPLY}x = "y"x ]
+then
+	echo "Start to package..."
+else
+	echo "Failed!"
+	exit
+fi
+
+cp -f ${DirLibStdcpp} ${DirLibs}
+cp -f ${DirLibJNILib} ${DirLibs}
+cp -f ${DirFaceLandmarks} ${DirLibs}
+
+tar czvf ${NamePackage} ${DirLibs}/*
+
+rm ${DirLibs}/${NameStdcpp}
+rm ${DirLibs}/${NameJNILib}
+rm ${DirLibs}/${NameFaceLandmarks}
+
+echo "Package success!"
 
 
 
