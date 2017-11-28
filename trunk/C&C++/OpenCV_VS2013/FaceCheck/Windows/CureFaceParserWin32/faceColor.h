9e03e5b27276939d8384a522a3414b12c0a6a417
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-11-28 07:18:33 +0000

    JNI库Windows平台

---
+++ trunk/C&C++/OpenCV_VS2013/FaceCheck/Windows/CureFaceParserWin32/faceColor.h
@@ -0,0 +1,26 @@
+﻿#ifndef __FACE_COLOR_H__
+#define __FACE_COLOR_H__
+
+#include "faceGlobal.h"
+
+typedef enum {
+	TouBai = 234,
+	BaiXi = 220,
+	ZiRan = 205,
+	XiaoMai = 164,
+	AnChen = 122,
+	YouHei = 81
+} enumFaceColorValue;
+
+typedef enum {
+	Type_Color_TouBai = 0,
+	Type_Color_BaiXi = 1,
+	Type_Color_ZiRan = 2,
+	Type_Color_XiaoMai = 3,
+	Type_Color_AnChen = 4,
+	Type_Color_YouHei = 5
+} enumFaceColorType;
+
+enumFaceColorType getFaceColorType(const std::string &strFile, const vectorContours &contours);
+
+#endif // __FACE_COLOR_H__
