648ae9658290c4c51a2c3cc23de6760e2a13e90b
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-12-01 01:47:15 +0000

    整合之后删除原来的Windows目录

--- C&C++/OpenCV_VS2013/FaceCheck/Windows/CureFaceParserWin32/faceSpotsDetect.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef __FACE_SPOTS_DETECT_H__
-#define __FACE_SPOTS_DETECT_H__
-
-#include "faceGlobal.h"
-
-bool findFaceSpots(const string &strImageName, const cv::Mat &matSrc, const vectorContours &faceContours, vectorInt &vectorIntResult);
-
-#endif // __FACE_SPOTS_DETECT_H__
-
