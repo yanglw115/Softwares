5c33e852c3b30b019388d1fa7d6881a5d08bf7b8
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-12-01 01:47:15 +0000

    整合之后删除原来的Windows目录

--- trunk/C&C++/OpenCV_VS2013/FaceCheck/Windows/faceGrabcut.h
+++
@@ -1,9 +0,0 @@
-#ifndef __GRABCUT_H__
-#define __GRABCUT_H__
-
-#include <opencv2/opencv.hpp>
-#include <iostream>
-using namespace std;
-
-int grabCut(const string strFile, cv::Rect rect);
-#endif 
--- trunk/C&C++/OpenCV_VS2013/FaceCheck/Windows/faceLandmarkDetect.h
+++
@@ -1,8 +0,0 @@
-﻿#ifndef  __FACE_LANDMARK_DETECT_H__
-#define __FACE_LANDMARK_DETECT_H__
-
-#include "faceGlobal.h"
-
-bool faceLandmarkDetect(const string &strImageName, const cv::Mat &, vectorContours &, cv::Rect &);
-
-#endif // ! __FACE_LANDMARK_DETECT_H__
