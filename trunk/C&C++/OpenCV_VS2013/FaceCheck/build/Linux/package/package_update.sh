e451a81885574375633cb5b48008efe0611162b0
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2017-12-05 03:11:31 +0000

    升级包打包与安装初步完成

---
+++ trunk/C&C++/OpenCV_VS2013/FaceCheck/build/Linux/package/package_update.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -o nounset  #脚本中禁止使用未定义的变量。或使用set -u
+set -o errexit  #脚本中任意一行执行错误时则退出。或使用set -e
+
+PWD=$(pwd)
+NameJNILib=libCureFaceRecognition.so
+NameUpdateShell=update.sh
+NamePackageUpdate=update.tar.gz
+NameUpdateBin=FaceParserUpdate
+NameProject=project
+
+DirLibJNILib=${PWD}/../${NameProject}/${NameJNILib}
+
+echo -e "\n"
+
+rm -rf ${PWD}/${NamePackageUpdate}
+rm -rf ${PWD}/${NameUpdateBin}*
+
+# 创建压缩包，压缩打包除安装脚本以外的所有文件
+tar czvf ${NamePackageUpdate} ${DirLibJNILib}
+
+# 将安装脚本和安装包cat到一起，生成.bin安装文件
+cat ${NameUpdateShell} ${NamePackageUpdate} > ${NameUpdateBin}
+chmod u+x ${NameUpdateBin}
+mv ${NameUpdateBin} ${NameUpdateBin}.$(date "+%Y%m%d_%H.%M.%S").bin
+
+rm -rf ${NamePackageUpdate}
+
+echo -e "Package update success!\n\n"
+
---
+++ trunk/C&C++/OpenCV_VS2013/FaceCheck/build/Linux/package/update.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -o nounset  #脚本中禁止使用未定义的变量。或使用set -u
+set -o errexit  #脚本中任意一行执行错误时则退出。或使用set -e
+
+echo -e "\n"
+# 需要用root权限
+if [ `id -u` -ne 0 ]; then
+    echo "Please use 'root' user, or run by 'sudo $0'"
+    exit 1
+else
+    echo "Start to update face parser tools and libraries, please wait..."
+fi
+
+PWD=$(pwd)
+NameProject=FaceParser
+NamePackageUpdate=update.tar.gz
+NameJNILib=libCureFaceRecognition.so
+
+DirInstall=/usr/local/${NameProject}
+DirInstallLibs=/usr/local/${NameProject}/libs
+
+if [ -d ${DirInstall} ]
+then
+	echo "..."
+else
+	echo "Oops! You may not install face parser tools or libraries before, cannot update!(Error)"
+	exit -1;
+fi
+
+# 提取安装包
+sed -n -e '1,/^###End###$/!p' $0 > ${DirInstall}/${NamePackageUpdate}
+
+# 解压缩安装包
+cd ${DirInstall}
+tar xvf ${NamePackageUpdate}
+
+# 更新faceParser库
+cp -f ${NameJNILib} ${DirInstallLibs}
+
+# 删除无用的文件
+rm -rf ${DirInstall}/${NamePackageUpdate}
+
+echo -e "\nCongratulations! Install face parser success!\n\n"
+
+exit 0
+#注意下面一行必须有且只能有一个换行符号
+###End###
+
+
