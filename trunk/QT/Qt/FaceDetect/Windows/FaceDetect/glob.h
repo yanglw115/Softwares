5e70328f63b739c8119e9e4955aa2acb30ce84da
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2018-02-07 04:01:03 +0000

    检测结果显示优化，使可以根据检测项目精准显示

--- trunk/QT/Qt/FaceDetect/Windows/FaceDetect/glob.h
+++ trunk/QT/Qt/FaceDetect/Windows/FaceDetect/glob.h
@@ -5,7 +5,7 @@ enum enumItemType {
     TYPE_ALL = 0xFF,
     TYPE_PIMPLES = 0x1,
     TYPE_BLACKHEADS = 0x2,
-    TYPE_FACE_COLORE = 0x4,
+    TYPE_FACE_COLOR = 0x4,
     TYPE_PORE = 0x8,
     TYPE_COARSENESS = 0x10
 };
