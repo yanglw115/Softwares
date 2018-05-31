eb916db44215a86b39fe82fcea33ba899dde82b0
yanglw115 <yanglw115@fdc7a43b-4700-4cfd-aa5a-fbf62e556ab2>
2018-05-31 09:22:26 +0000

    1、TreeView数据从数据库读取功能实现； 2、数据库使用全局变量，这样各处可以灵活地访问同一个数据库，避免数据库的打开与关闭等重复操作；

--- QT/Qt/CureOffice/cure_global.h
+++ QT/Qt/CureOffice/cure_global.h
@@ -2,12 +2,19 @@
 #define CURE_GLOBAL
 
 #include <QString>
+#include <QSqlDatabase>
+#include <QSqlDriver>
+#include <QSqlError>
+#include <QSqlQuery>
+#include <QSqlRecord>
 
 #define SALARY_CHECKED 0x01
 #define SALARY_SEND_OK 0x02
 
+extern QSqlDatabase g_db;
 extern const QString g_strDataDir;
 extern const QString g_strDatabaseFile;
+extern const QString g_strTableNameStructure;
 
 #endif // CURE_GLOBAL
 
